40918a513e473bb11074a66839622b7b98ba2fad
David Hierholz <hierholz@mail.hs-ulm.de>
2017-12-07 15:16:59 +0100

    Added ShowQRActivity.

--- Client/Chattr/app/src/main/java/com/example/david/chattr/NewManuallContactActivity.java
+++ Client/Chattr/app/src/main/java/com/example/david/chattr/NewManuallContactActivity.java
@@ -1,33 +1,24 @@
 package com.example.david.chattr;
 
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.support.annotation.Nullable;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-
-import com.example.david.chattr.mainactivity_fragments.PagerAdapter;
-import com.example.david.chattr.mqtt_chat.MyMqttService;
 
 
-public class NewContactActivity extends AppCompatActivity {
+public class NewManuallContactActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.new_contact);
+        setContentView(R.layout.new_manuall_contact);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("New Contact");
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        //Todo: Functionality to Save Contact
     }
 
     //for the back button
--- Client/Chattr/app/src/main/java/com/example/david/chattr/mainactivity_fragments/NewContactFragment.java
+++ Client/Chattr/app/src/main/java/com/example/david/chattr/mainactivity_fragments/NewContactFragment.java
@@ -8,17 +8,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.ListView;
 
-import com.example.david.chattr.ChatActivity;
-import com.example.david.chattr.MainActivity;
-import com.example.david.chattr.NewContactActivity;
+import com.example.david.chattr.NewManuallContactActivity;
 import com.example.david.chattr.R;
-import com.example.david.chattr.entities.users.UserProfile;
-
-import java.util.ArrayList;
+import com.example.david.chattr.ShowQrActivity;
 
 /**
  * Created by david on 15.11.17.
@@ -31,12 +25,40 @@ public class NewContactFragment extends Fragment{
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_new_contact, container, false);
 
+        // The differennt buttons to chose how to add a new contact
         Button manuallButton = (Button) view.findViewById(R.id.manuallButton);
+        Button nfcButton = (Button) view.findViewById(R.id.nfcButton);
+        Button qrButton = (Button) view.findViewById(R.id.qrButton);
+        Button showQrButton = (Button) view.findViewById(R.id.showQrButton);
 
+        // The button click listeners start new Activitys
+        // Add Contact manually
         manuallButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), NewContactActivity.class);
+                Intent intent = new Intent(getActivity(), NewManuallContactActivity.class);
+                startActivity(intent);
+            }
+        });
+        // Add Contact with NFC
+        nfcButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO: Start NFC reader
+            }
+        });
+        // Add Contact with QR
+        qrButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO: Start camera to read qr code
+            }
+        });
+        // Show your own QR-Code
+        showQrButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), ShowQrActivity.class);
                 startActivity(intent);
             }
         });
